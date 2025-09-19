fa8ccb027edf6ede70d99a6f33098a871e682ddd
Mike Reiche <mike@reiche.world>
2025-09-19 10:38:00 +0200

    Add CLI script

--- owasp_dt_sync/cli.py
+++ owasp_dt_sync/cli.py
@@ -1,7 +1,7 @@
 from owasp_dt_sync.args import create_parser
 from owasp_dt_sync.log import logger
 
-def run():
+def main():
     parser = create_parser()
     try:
         args = parser.parse_args()
@@ -12,6 +12,3 @@ def run():
     except Exception as e:
         logger.exception(e)
         exit(1)
-
-if __name__ == "__main__":  # pragma: no cover
-    run()  # pragma: no cover
