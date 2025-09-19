55ccf5fcb50cd024e92d290e1d3b87601b7abffd
Mike Reiche <mike@reiche.world>
2025-09-19 10:56:32 +0200

    Bugfix/pypi package (#14)
    * Migrate to pyproject.toml
    
    * Add CLI script
    
    * Fix main call

--- owasp_dt_sync/cli.py
+++ owasp_dt_sync/cli.py
@@ -1,7 +1,7 @@
 from owasp_dt_sync.args import create_parser
 from owasp_dt_sync.log import logger
 
-def run():
+def main():
     parser = create_parser()
     try:
         args = parser.parse_args()
@@ -14,4 +14,4 @@ def run():
         exit(1)
 
 if __name__ == "__main__":  # pragma: no cover
-    run()  # pragma: no cover
+    main()  # pragma: no cover
